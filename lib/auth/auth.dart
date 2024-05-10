0063661b2c8ff87e9a4ed6d50943ed47c3bf6aa7
FITurnip <franklinirtr@gmail.com>
2024-05-10 20:14:48 +0700

    fixing login

--- lib/auth/auth.dart
+++ lib/auth/auth.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:kuis4/item/items.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class LoginRegisterPage extends StatefulWidget {
@@ -10,6 +9,7 @@ class LoginRegisterPage extends StatefulWidget {
 
 class _LoginRegisterPageState extends State<LoginRegisterPage> {
   bool _isLoginForm = true;
+  bool _isLoading = false; // Add loading state variable
 
   void _toggleForm() {
     setState(() {
@@ -25,40 +25,64 @@ class _LoginRegisterPageState extends State<LoginRegisterPage> {
       ),
       body: Center(
         child: _isLoginForm
-            ? LoginForm(toggleForm: _toggleForm)
+            ? LoginForm(
+                toggleForm: _toggleForm,
+                isLoading: _isLoading, // Pass loading state to LoginForm
+                onLogin: _login, // Pass login method to LoginForm
+              )
             : RegisterForm(toggleForm: _toggleForm),
       ),
     );
   }
-}
 
-class LoginForm extends StatelessWidget {
-  final VoidCallback toggleForm;
+  void _login(String username, String password) async {
+    setState(() {
+      _isLoading = true; // Set loading state to true when login process starts
+    });
 
-  LoginForm({required this.toggleForm});
-
-  //tambahan dari yt
-  TextEditingController usernameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
-  void login(String username, password) async {
     try {
-      Response response = await post(
-          Uri.parse('http://146.190.109.66:8000/login'),
-          body: {'username': username, 'password': password});
+      final response = await http.post(
+        Uri.parse("http://192.168.100.36:8000/login"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: jsonEncode(
+            {"username": username, "password": password}),
+      );
 
       if (response.statusCode == 200) {
         var data = jsonDecode(response.body.toString());
 
         print(data);
         print('Login successfully');
+        // Navigator.of(context).push(MaterialPageRoute(builder: (context) => FoodListPage()));
       } else {
         print('Gagal Login');
       }
     } catch (e) {
       print(e.toString());
+    } finally {
+      setState(() {
+        _isLoading = false; // Set loading state to false when login process ends
+      });
     }
   }
+}
+
+class LoginForm extends StatelessWidget {
+  final VoidCallback toggleForm;
+  final bool isLoading; // Add loading state variable
+  final Function(String, String) onLogin; // Add login method
+
+  // Add loading state variable and login method to constructor
+  LoginForm({
+    required this.toggleForm,
+    required this.isLoading,
+    required this.onLogin,
+  });
+
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -78,15 +102,15 @@ class LoginForm extends StatelessWidget {
           ),
           SizedBox(height: 20.0),
           ElevatedButton(
-            onPressed: () {
-              login(usernameController.text.toString(),
-                  passwordController.text.toString());
-
-              // // Implement login functionality here
-              // Navigator.of(context).push(
-              //     MaterialPageRoute(builder: (context) => FoodListPage()));
-            },
-            child: Text('Login'),
+            onPressed: isLoading // Disable button when loading
+                ? null
+                : () => onLogin(
+                    usernameController.text.toString(),
+                    passwordController.text.toString(),
+                  ),
+            child: isLoading // Show loading indicator if loading
+                ? CircularProgressIndicator()
+                : Text('Login'),
           ),
           SizedBox(height: 20.0),
           TextButton(
